b0a19d43851a77cfccef85caa4240582a5bf74a1
Sean Sundberg <seansund@us.ibm.com>
2020-06-25 10:20:50 -0500

    Updates jaeger-operator to v0.3.0 and limits retries to 12 in wait-for-deployment

--- scripts/wait-for-deployments.sh
+++ scripts/wait-for-deployments.sh
@@ -4,9 +4,17 @@ NAMESPACE="$1"
 NAME="$2"
 
 count=0
-while [[ $(kubectl get deployment -n "${NAMESPACE}" -l "app=${NAME}" | wc -l) -gt 2 ]] && [[ "$count" -lt 10 ]]; do
+while [[ $(kubectl get deployment -n "${NAMESPACE}" -l "app=${NAME}" | wc -l) -gt 2 ]]; do
+  if [[ "$count" -eq 12 ]]; then
+    echo "Timed out waiting for deployments in ${NAMESPACE}"
+    kubectl get deployment -n "${NAMESPACE}" -l "app=${NAME}"
+    exit 1
+  fi
+
   echo "Waiting for deployments in ${NAMESPACE} namespace"
   sleep 30
+
+  count=$((count+1))
 done
 
 DEPLOYMENTS=$(kubectl get deployment -n "${NAMESPACE}" -l "app=${NAME}" -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
