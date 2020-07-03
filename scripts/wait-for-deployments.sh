213248b2885ea64e8b74036b542130acf748c0f5
Sean Sundberg <seansund@us.ibm.com>
2020-07-03 15:48:50 -0500

    Fixes deployment name

--- scripts/wait-for-deployments.sh
+++ scripts/wait-for-deployments.sh
@@ -4,20 +4,20 @@ NAMESPACE="$1"
 NAME="$2"
 
 count=0
-while [[ $(kubectl get deployment -n "${NAMESPACE}" -l "app=${NAME}" | wc -l) -gt 2 ]]; do
+until kubectl get deployment -n "${NAMESPACE}" "${NAME}"; do
   if [[ "$count" -eq 12 ]]; then
-    echo "Timed out waiting for deployments in ${NAMESPACE}"
-    kubectl get deployment -n "${NAMESPACE}" -l "app=${NAME}"
+    echo "Timed out waiting for deployment/${NAME} in ${NAMESPACE}"
+    kubectl get deployment -n "${NAMESPACE}" "${NAME}"
     exit 1
   fi
 
-  echo "Waiting for deployments in ${NAMESPACE} namespace"
+  echo "Waiting for deployment/${NAME} in ${NAMESPACE} namespace"
   sleep 30
 
   count=$((count+1))
 done
 
-DEPLOYMENTS=$(kubectl get deployment -n "${NAMESPACE}" -l "app=${NAME}" -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
+DEPLOYMENTS="${NAME}"
 
 echo "${DEPLOYMENTS}" | while read DEPLOYMENT; do
   count=0
